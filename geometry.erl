f8bce15d541a48de1f54b5b681f282d25ef12383
Gourab Mitra <gourab@Gourabs-MacBook-Pro.local>
2018-08-28 11:31:11 -0700

    chq1

--- geometry.erl
+++ geometry.erl
@@ -1,6 +1,14 @@
 -module(geometry).
--export([area/1]).
+-export([area/1, perimeter/1, test/0]).
+
+test() ->
+    144 = area({square, 12}), % TODO Ask Mark about usage of comma, difference with ;
+    tests_worked.
 
 area({circle, Radius}) -> math:pi() * Radius * Radius;
 area({square, Side}) -> Side * Side;
 area({rectangle, Width, Height}) -> Width * Height.
+
+perimeter({circle, Radius}) -> math:pi() * Radius * 2.0;
+perimeter({square, Side}) -> 4.0 * Side;
+perimeter({rectangle, Width, Height}) -> 2 * (Width + Height).
